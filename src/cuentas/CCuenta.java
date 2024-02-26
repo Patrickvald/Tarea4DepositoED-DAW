d7d71b4141a68df69c64c86bca9a815f86e53994
Patrick Valdivia <patrick.v.valdivia@gmail.com>
2024-02-26 10:48:06 +0100

    Segundo commit para el punto 6 de la tarea.

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,13 +1,21 @@
 package cuentas;
-
+/**
+ * Clase para instanciar cuentas
+ * @author Patrick Valdivia
+ */
 public class CCuenta {
 
+    /**
+     * Creación de atributos
+     */
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+/**
+ * Constructores de la clase CCuenta
+ */
     public CCuenta()
     {
     }
@@ -19,6 +27,10 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Encapsulado de los atributos vía getter/setter
+     * @return 
+     */
     public String getNombre() {
         return nombre;
     }
@@ -51,19 +63,32 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
     
-    
+    /**
+     * Método para obtener el estado de la cuenta
+     * @return 
+     */
 
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Método para ingresar dinero en la cuenta
+     * @param cantidad
+     * @throws Exception si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
+    /**
+     * Método para extraer dinero de la cuenta
+     * @param cantidad
+     * @throws Exception si la cantidad es negativa
+     */
 
     public void retirar(double cantidad) throws Exception
     {
